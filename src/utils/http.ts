42866dcb480c7f007e869edda9e7db3cfd92e6a9
Tran Pham Thanh Nam <trannam.nt865@gmail.com>
2024-12-09 05:50:14 +0700

    Update http.ts

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,5 @@
-// const baseUrl: string = 'https://api.jas.id.vn/api/'
+const baseUrl: string = 'https://api.jas.id.vn/api/'
 export const baseUrlWithoutAPI: string = 'https://api.jas.id.vn'
 
-const baseUrl: string = 'http://103.163.25.53/api/'
+// const baseUrl: string = 'http://103.163.25.53/api/'
 export default baseUrl
